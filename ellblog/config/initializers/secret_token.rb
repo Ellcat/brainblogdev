6a1c4831532a9210df42471fb71bd1a1cc29b762
vagrant <vagrant@precise32.(none)>
2015-08-03 12:43:28 +0000

    Initial commit

---
+++ ellblog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Ellblog::Application.config.secret_key_base = '54afe20a2a91ac9f3c0eeb61b39e89ca45a2fc800eec9af790b4431345c5b667c21595b3594749742f2f40f8a82b498f0ecf548e1cfd767e23ddb73da5124357'
